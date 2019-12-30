2bdf45bff68203ef14dc4d7db34d4cc711ab0edc
Ozan Seymen <oseymen@gmail.com>
2019-12-30 10:56:35 +0000

    query strings

--- src/GetHandler.hs
+++ src/GetHandler.hs
@@ -1,19 +1,31 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module GetHandler where
 
-import qualified Web.Scotty as S
-import qualified Data.Text.Lazy as L
-import Data.Aeson
-import Data.Aeson.Types as AT
+import           Data.Aeson
+import           Data.Aeson.Types as AT
+import qualified Data.Text.Lazy   as L
+import           GHC.Generics
+import qualified Web.Scotty       as S
+
+data Response = Response {
+                            args    :: QueryStrings,
+                            headers :: Headers
+                         } deriving (Generic)
+
+instance ToJSON Response
 
 newtype Headers = Headers [(L.Text, L.Text)]
-                deriving (Show)
+  deriving (Show)
 
 instance ToJSON Headers where
-  toJSON (Headers hs) = do
-    let obj = object $ map lazyTextToJsonPair hs
-    object ["headers" .= obj]
+  toJSON (Headers hs) = object $ map lazyTextToJsonPair hs
+
+newtype QueryStrings = QueryStrings [(L.Text, L.Text)]
+  deriving (Show)
+
+instance ToJSON QueryStrings where
+  toJSON (QueryStrings qs) = object $ map lazyTextToJsonPair qs
 
 lazyTextToJsonPair :: (L.Text, L.Text) -> AT.Pair
 lazyTextToJsonPair (a, b) = L.toStrict a .= b
@@ -21,4 +33,5 @@ lazyTextToJsonPair (a, b) = L.toStrict a .= b
 getHandler :: S.ActionM ()
 getHandler = do
   headers <- S.headers
-  S.json $ Headers headers
+  params <- S.params
+  S.json $ Response { args = QueryStrings params, headers = Headers headers }
